bf5c9fd27754a120475aeb0a9bc6ab406be8620d
alex <alextotalk@gmail.com>
2020-03-16 13:42:15 +0200

    Updates

---
+++ precache-manifest.c1acf9ae800db0297d71c2bb8c8f254b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e322d32361f14e1204a128f11f3a8da1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "656ad671ca49dd8a2e03",
+    "url": "/portfolio/static/css/main.ce7abf88.chunk.css"
+  },
+  {
+    "revision": "3d0becdd1c48c700b891",
+    "url": "/portfolio/static/js/2.5dd35f00.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.5dd35f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "656ad671ca49dd8a2e03",
+    "url": "/portfolio/static/js/main.64e6ac90.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "3addfbd06a04768729658c0d1e7c0dbe",
+    "url": "/portfolio/static/media/mi3.3addfbd0.jpg"
+  },
+  {
+    "revision": "ebb651ec227e755646be6e6e04d2eb88",
+    "url": "/portfolio/static/media/Колясик2.ebb651ec.png"
+  }
+]);
