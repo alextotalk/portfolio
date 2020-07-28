45b60caff0375efde6650bfc1dd7fa7375db34b8
alex <alextotalk@gmail.com>
2020-07-29 00:54:01 +0300

    end

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Contacts from "./components/Contacts/Contacts";
 import Footer from "./components/Footer/Footer";
+import {Helmet} from "react-helmet";
+
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Aleksei Zaiats</title>
+        </Helmet>
       <div className="portfolio">
         <Header />
         <Main />
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,12 +5,12 @@ import Project from "../Projects/Project";
 function Projects() {
     const projectsProp = [
         {id: '01', name:'IT-samurai-game', description: 'Click to Joker and fun', href: 'http://alexooua.github.io/game-kiss'},
-        {id: '02', name:'TodoList', description: 'Take a note for the future', href: 'https://fitkovich1.github.io/todolist13_study/'},
-        {id: '02', name:'Counter', description: 'Change start value and count number up or down', href: 'https://fitkovich1.github.io/CounterRedux/'},
+        // {id: '02', name:'TodoList', description: 'Take a note for the future', href: 'https://fitkovich1.github.io/todolist13_study/'},
+        {id: '02', name:'Counter', description: 'Change start value and count number up or down', href: 'http://alexooua.github.io/counter'},
         // {id: '03', name:'Tic-tac-toe', description: 'Сlassic tic tac toe game', href: 'https://fitkovich1.github.io/tic-tac-toe/'},
-        {id: '04', name: 'Calculator', description: 'Сount complex arithmetic expressions', href: 'https://fitkovich1.github.io/'},
-        {id: '05', name: 'Stopwatch', description: 'Measure the time of any action', href: 'http://alexooua.github.io/stop-watch'},
-        {id: '06', name: 'Social network', description:'Communication between people', href: 'http://alexooua.github.io/social'}
+        {id: '03', name: 'Calculator', description: 'Сount complex arithmetic expressions', href: 'http://alexooua.github.io/calculator'},
+        {id: '04', name: 'Stopwatch', description: 'Measure the time of any action', href: 'http://alexooua.github.io/stop-watch'},
+        {id: '05', name: 'Social network', description:'Communication between people', href: 'http://alexooua.github.io/social'}
 
     ];
     let projects = projectsProp.map((pr) => <Project id={pr.id} name={pr.name} description={pr.description} href={pr.href}/>)
