0a8cc7444f0a5c1acfae1c209c08250d83a0b071
alex <alextotalk@gmail.com>
2020-07-28 11:55:35 +0300

    end

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,13 +4,13 @@ import Project from "../Projects/Project";
 
 function Projects() {
     const projectsProp = [
-        {id: '01', name:'Joker-game', description: 'Click to Joker and fun', href: 'https://fitkovich1.github.io/dog-game/'},
-        /*{id: '02', name:'TodoList', description: 'Take a note for the future', href: 'https://fitkovich1.github.io/todolist13_study/'},*/
+        {id: '01', name:'IT-samurai-game', description: 'Click to Joker and fun', href: 'http://alexooua.github.io/game-kiss'},
+        {id: '02', name:'TodoList', description: 'Take a note for the future', href: 'https://fitkovich1.github.io/todolist13_study/'},
         {id: '02', name:'Counter', description: 'Change start value and count number up or down', href: 'https://fitkovich1.github.io/CounterRedux/'},
-        {id: '03', name:'Tic-tac-toe', description: 'Сlassic tic tac toe game', href: 'https://fitkovich1.github.io/tic-tac-toe/'},
+        // {id: '03', name:'Tic-tac-toe', description: 'Сlassic tic tac toe game', href: 'https://fitkovich1.github.io/tic-tac-toe/'},
         {id: '04', name: 'Calculator', description: 'Сount complex arithmetic expressions', href: 'https://fitkovich1.github.io/'},
-        {id: '05', name: 'Stopwatch', description: 'Measure the time of any action', href: 'https://fitkovich1.github.io/stopWatch-fitkovich1.github.io/'},
-        {id: '06', name: 'Social network', description:'Communication between people', href: 'https://fitkovich1.github.io/socialNetwork/'}
+        {id: '05', name: 'Stopwatch', description: 'Measure the time of any action', href: 'http://alexooua.github.io/stop-watch'},
+        {id: '06', name: 'Social network', description:'Communication between people', href: 'http://alexooua.github.io/social'}
 
     ];
     let projects = projectsProp.map((pr) => <Project id={pr.id} name={pr.name} description={pr.description} href={pr.href}/>)
